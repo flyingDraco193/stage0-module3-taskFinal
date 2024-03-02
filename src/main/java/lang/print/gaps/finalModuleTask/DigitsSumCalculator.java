a08975d1221f5e8403adfe81a805607f5c1458db
Gempton <gemptonilya@gmail.com>
2024-03-02 19:26:33 +0300

    Done all tasks

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int fourthDigit = number % 10;
+        int thirdDigit = number % 100 / 10;
+        int secondDigit = number % 1000 / 100;
+        int firstDigit = number / 1000;
+
+        int sum = firstDigit + secondDigit + thirdDigit + fourthDigit;
+
+        System.out.println(sum);
     }
 }
